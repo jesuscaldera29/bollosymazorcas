d1eae3b34b7c9bb140524f92bf721247eec56f36
jesuscaldera29 <113543931+jesuscaldera29@users.noreply.github.com>
2025-12-14 11:42:44 -0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "bollos-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./db.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(req).then(cached => cached || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      }).catch(() => cached))
+    );
+  }
+});
